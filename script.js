31c055ceee6f730c18b6bd051d750265d22e50dc
Am768 <sukkurcite38@gmail.com>
2026-03-10 18:44:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    var menu = document.getElementById("nav-menu");
+
+    if(menu.style.display === "flex"){
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}<a href="#about" onclick="closeMenu()">About</a>
